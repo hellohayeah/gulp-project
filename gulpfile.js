737bfefd8f23efb2d4964acf43a5ac1c864051bc
蔡旻勳 <hellohayeah@gmail.com>
2018-05-19 16:45:15 +0800

    add gulp header and css @charset "UTF-8";

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var header = require('gulp-header');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
@@ -8,7 +9,7 @@ var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create(); // 載入 browser-sync
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   return browserSync.init({
     server: {
       baseDir: './dev' // 要瀏覽器同步的資料夾
@@ -17,27 +18,38 @@ gulp.task('browserSync', function() {
   });
 })
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   var postcss = require('gulp-postcss');
   var autoprefixer = require('autoprefixer');
-  var processors = [autoprefixer({ browsers: ['last 2 version'] })];
+  var processors = [autoprefixer({
+    browsers: ['last 2 version']
+  })];
   return gulp.src('src/sass/*.sass')
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')})) // 出錯則顯示提示視窗，不中斷。
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    })) // 出錯則顯示提示視窗，不中斷。
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
     .pipe(postcss(processors))
+    .pipe(header('@charset "UTF-8";'))
     .pipe(gulp.dest('./dev/css'))
 });
 
-gulp.task('pug', function() {
+gulp.task('pug', function () {
   return gulp.src('src/pug/*.pug')
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')})) // 出錯則顯示提示視窗，不中斷。
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    })) // 出錯則顯示提示視窗，不中斷。
     .pipe(pug())
     .pipe(gulp.dest('./dev'))
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src('src/js/**/*.js')
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')})) // 出錯則顯示提示視窗，不中斷。
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    })) // 出錯則顯示提示視窗，不中斷。
     .pipe(babel({
       presets: ['env']
     }))
@@ -45,7 +57,7 @@ gulp.task('js', function() {
     .pipe(gulp.dest('dev/js'))
 });
 
-gulp.task('img', function() {
+gulp.task('img', function () {
   return gulp.src('src/img/**/*')
     .pipe(imagemin({
       optimizationLevel: 3,
@@ -55,7 +67,7 @@ gulp.task('img', function() {
     .pipe(gulp.dest('./dev/img'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'));
   gulp.watch('src/pug/**/*.pug', gulp.parallel('pug'))
   gulp.watch('src/js/**/*.js', gulp.parallel('js'));
